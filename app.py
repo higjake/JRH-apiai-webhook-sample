f4bf9b89c997c1f6f058fa5c737dfff21881b99a
higjake <higjake@gmail.com>
2017-03-31 15:53:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "expertiseProfessionSearch":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://www.expertise.com/api/v1.0/directories/nc/charlotte/yoga-studios"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    yql_url = baseurl + urlencode({yql_query})
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
