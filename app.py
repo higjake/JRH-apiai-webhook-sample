4f81c87d00a6d370fb458e4a6395130cd7b46071
higjake <higjake@gmail.com>
2017-04-05 08:31:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,109 +1,73 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
-
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
-
 import json
 import os
-
 from flask import Flask
 from flask import request
 from flask import make_response
-
 # Flask app should start in global layout
 app = Flask(__name__)
-
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
     print("Request:")
     print(json.dumps(req, indent=4))
-
     res = processRequest(req)
-
     res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
+   # print(res)
+   r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
-
 def processRequest(req):
     if req.get("result").get("action") != "expertiseProfessionSearch":
-        return {}
-    baseurl = "https://requestb.in/16op1y91?"
+        return { "no match" }
+    baseurl = "https://www.expertise.com/api/v1.0/directories/"
     url_query = makeQuery(req)
     if url_query is None:
         return {}
-    final_url = baseurl + urlencode({url_query}) + "&format=json"
+   #final_url = baseurl + urlencode({url_query})
+   final_url = "https://www.expertise.com/api/v1.0/directories/ga/atlanta/flooring"
     result = urlopen(final_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
-
-
 def makeQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    state = parameters.get("state")
-    city = parameters.get("geo-city")
-    vert = parameters.get("profession")
-    if state is None:
+   result = req.get("result")
+   parameters = result.get("parameters")
+   state = parameters.get("state")
+   city = parameters.get("city")
+   vert = parameters.get("profession")
+   if state is None:
         return None
-    
     return state + "/" + city + "/" + vert
 
-
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+    providers = data.get('providers')
+    if providers is None:
+        return {
+       "speech": "Not Today",
+       "displayText": "Not Today",
+       # "data": data,
+       # "contextOut": [],
+       "source": "apiai-weather-webhook-sample"
+   }
+# print(json.dumps(item, indent=4))
+   speech = "Today in" + providers.get('business_name')
     print("Response:")
     print(speech)
-
     return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-
-
+       "speech": speech,
+       "displayText": speech,
+       # "data": data,
+       # "contextOut": [],
+       "source": "apiai-weather-webhook-sample"
+   }
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
     print("Starting app on port %d" % port)
-
     app.run(debug=False, port=port, host='0.0.0.0')
