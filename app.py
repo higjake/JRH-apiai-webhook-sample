cf10d31f6dd79acfb6f27b5eacbc058346ac9cd1
higjake <higjake@gmail.com>
2017-04-05 09:09:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def processRequest(req):
     url_query = makeQuery(req)
     if url_query is None:
         return {}
-    #final_url = baseurl + urlencode({url_query})
-    final_url = "https://www.expertise.com/api/v1.0/directories/ga/atlanta/flooring"
+    final_url = baseurl + urlencode({url_query})
+    #final_url = "https://www.expertise.com/api/v1.0/directories/ga/atlanta/flooring"
     result = urlopen(final_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
